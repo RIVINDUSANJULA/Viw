b0156f61d57612bd45d8a492a1227addcd1cc935
Rivindu Sanjula <rivindusanjula194@gmail.com>
2026-03-01 19:41:00 +0530

    Working --> context - Bug Fix - V0.2

---
+++ viw/src/context/AuthContext.tsx
@@ -0,0 +1,54 @@
+import { createContext, useContext, useEffect, useState, type ReactNode } from 'react'
+
+import { type User, type Session } from "@supabase/supabase-js";
+import { supabase } from "../lib/supabase";
+import { error } from 'console';
+
+interface AuthContextType {
+  user: User | null;
+  session: Session | null;
+  loading: boolean;
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export default function AuthProvider({ children }: { children: ReactNode }) {
+
+    const [user, setUser] = useState<User | null>(null);
+    const [session, setSession] = useState<Session | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            if (error) {
+                console.error("Error fetching session:", error.message);
+            }
+        setSession(session);
+        setUser(session?.user ?? null);
+        setLoading(false);
+        });
+
+        const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
+        setSession(session);
+        setUser(session?.user ?? null);
+        });
+
+        return () => subscription.unsubscribe();
+    }, []);
+
+
+  return (
+    <AuthContext.Provider value={{ user, session, loading }}>
+      {/* {!loading && children} */}
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) throw new Error("useAuth must be used within an AuthProvider");
+  return context;
+}
